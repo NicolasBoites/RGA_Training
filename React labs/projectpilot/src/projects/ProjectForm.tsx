754db544c5bafe525992d6746eaf6645b77008e1
Nicolas Boites <234995@ust.com>
2025-05-21 16:48:20 +0000

    Lab 16 Form Validation

--- React labs/projectpilot/src/projects/ProjectForm.tsx
+++ React labs/projectpilot/src/projects/ProjectForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { use } from 'react'
 import { SyntheticEvent, useState } from 'react';
 import { Project } from './Project';
 
@@ -13,10 +13,7 @@ function ProjectForm(props: ProjectFormProps) {
     const { onCancel, onSave, project: initialProject } = props;
 
     const [project, setProject] = useState(initialProject);
-    // const [name, setName] = useState(initialProject.name || '');
-    // const [description, setDescription] = useState(initialProject.description || '');
-    // const [budget, setBudget] = useState(initialProject.budget || 0);
-    // const [isActive, setIsActive] = useState(initialProject.isActive || 0);
+    const [errors, setErrors] = useState({ name: '', description: '', budget: '' });
 
     const handlerCancelClick = () => {
         onCancel()
@@ -24,7 +21,34 @@ function ProjectForm(props: ProjectFormProps) {
 
     const handlerSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
+        if (!isValid()) return;
         onSave(new Project(project))
+    };
+
+    function validate(project: Project) {
+
+        let errors: any = { name: '', description: '', budget: '' };
+        if (project.name.length === 0) {
+            errors.name = 'Name is required';
+        }
+        if (project.name.length > 0 && project.name.length < 3) {
+            errors.name = 'Name needs to be at least 3 characters.';
+        }
+        if (project.description.length === 0) {
+            errors.description = 'Description is required.';
+        }
+        if (project.budget === 0) {
+            errors.budget = 'Budget must be more than $0.';
+        }
+        return errors;
+    }
+
+    function isValid() {
+        return (
+            errors.name.length === 0 &&
+            errors.description.length === 0 &&
+            errors.budget.length === 0
+        );
     }
 
     const handleChange = (event: any) => {
@@ -51,35 +75,57 @@ function ProjectForm(props: ProjectFormProps) {
             updatedProject = new Project({ ...p, ...change });
             return updatedProject;
         });
+        setErrors(() => validate(updatedProject));
     };
 
     return (
-        <div><form className="input-group vertical" onSubmit={handlerSubmit}>
-            <label htmlFor="name">Project Name</label>
-            <input type="text" name="name" placeholder="enter name"
-                value={project.name}
-                onChange={handleChange} />
-            <label htmlFor="description">Project Description</label>
+        <div>
 
-            <textarea name="description" placeholder="enter description"
-                value={project.description}
-                onChange={handleChange} ></textarea>
-            <label htmlFor="budget">Project Budget</label>
+            <form className="input-group vertical" onSubmit={handlerSubmit}>
+                <label htmlFor="name">Project Name</label>
+                {errors.name.length > 0 && (
+                    <div className="card error">
+                        <p>{errors.name}</p>
+                    </div>
+                )}
+                <input type="text" name="name" placeholder="enter name"
+                    value={project.name}
+                    onChange={handleChange} />
 
-            <input type="number" name="budget" placeholder="enter budget"
-                value={project.budget}
-                onChange={handleChange} />
-            <label htmlFor="isActive">Active?</label>
-            <input type="checkbox" name="isActive"
-                checked={project.isActive}
-                onChange={handleChange} />
 
-            <div className="input-group">
-                <button className="primary bordered medium">Save</button>
-                <span></span>
-                <button type="button" className="bordered medium" onClick={handlerCancelClick}>cancel</button>
-            </div>
-        </form></div>
+                <label htmlFor="description">Project Description</label>
+                {errors.description.length > 0 && (
+                    <div className="card error">
+                        <p>{errors.description}</p>
+                    </div>
+                )}
+                <textarea name="description" placeholder="enter description"
+                    value={project.description}
+                    onChange={handleChange} ></textarea>
+
+
+                <label htmlFor="budget">Project Budget</label>
+                {errors.budget.length > 0 && (
+                    <div className="card error">
+                        <p>{errors.budget}</p>
+                    </div>
+                )}
+                <input type="number" name="budget" placeholder="enter budget"
+                    value={project.budget}
+                    onChange={handleChange} />
+
+
+                <label htmlFor="isActive">Active?</label>
+                <input type="checkbox" name="isActive"
+                    checked={project.isActive}
+                    onChange={handleChange} />
+
+                <div className="input-group">
+                    <button className="primary bordered medium">Save</button>
+                    <span></span>
+                    <button type="button" className="bordered medium" onClick={handlerCancelClick}>cancel</button>
+                </div>
+            </form></div>
     )
 }
 
